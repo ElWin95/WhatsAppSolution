8d4ece7e304e3c97be1439997ee8550396746a86
ElWin95 <elvinma@code.edu.az>
2023-02-25 22:43:59 +0400

    WhatsApp

---
+++ WhatsApp/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace WhatsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Massivin uzunlugunu daxil edin:");
+            int n = int.Parse(Console.ReadLine());
+            int[] number = new int[n];
+
+            for (int i = 0; i < number.Length; i++)
+            {
+                Console.Write($"Number[{i}]=");
+                number[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < number.Length; i++)
+            {
+                if (number[i] % 2 == 0)
+                {
+                    Console.WriteLine(number[i]);
+                }
+            }
+        }
+    }
+}
